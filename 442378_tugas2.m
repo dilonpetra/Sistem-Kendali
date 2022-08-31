ce2d44c3666a08d8215a00fc0f199fbf26f55cda
L. Odilon Petra I <65167232+dilonpetra@users.noreply.github.com>
2022-08-31 09:01:58 +0700

    Add files via upload

---
+++ 442378_tugas2.m
@@ -0,0 +1,20 @@
+s = tf('s');
+
+T = 10;
+
+n=1;
+den=[T T/16 1];
+G = tf(n,den);
+figure(1)
+step(G);
+figure(2)
+impulse(G);
+figure(3)
+step(G/s); 
+xlim([0 50]);
+
+
+s1 = stepinfo(G);
+disp(s1);
+[y,t]=step(G);
+sserror=(1-y(end));
